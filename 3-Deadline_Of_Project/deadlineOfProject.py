f208c5d656147f1d389bcedc518f6e0129dce741
bartodes <bartojdes@gmail.com>
2023-03-31 17:56:14 -0300

    initial commit

---
+++ 3-Deadline_Of_Project/deadlineOfProject.py
@@ -0,0 +1,17 @@
+import datetime
+
+def resultsForUser():
+    print("output")
+
+def calculateDeadline():
+    print("deadline")
+
+def validateInput():
+    print("validation")
+
+def deadlineInput():
+    user_input= input("What's your project and it's deadline? (format: <project>:dd/mm/yy)\n")
+
+def main():
+    deadlineInput()
+main()
